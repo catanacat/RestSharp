81d01a90d6667d49e2b26eb8a6f7aea86e50afcb
catanacat <catanacat@yahoo.com>
2015-07-14 18:21:20 +0200

    Revert "support for Dictionaries in JsonDeserializer"
    This reverts commit 449fcad543e369ab7a4748463b2ef4721bc0ca24.
    
    Conflicts:
    	RestSharp.Tests/JsonTests.cs
    	RestSharp.Tests/NamespacedXmlTests.cs
    	RestSharp.Tests/SampleClasses.cs
    	RestSharp.Tests/XmlTests.cs
    	RestSharp/Deserializers/JsonDeserializer.cs
    
    dddddddddddddd

--- RestSharp.Tests/JsonTests.cs
+++ RestSharp.Tests/JsonTests.cs
@@ -25,6 +25,7 @@ using Xunit;
 
 namespace RestSharp.Tests
 {
+<<<<<<< HEAD
     public class JsonTests
     {
         private const string AlternativeCulture = "pt-PT";
@@ -965,4 +966,71 @@ namespace RestSharp.Tests
             return d.Deserialize<T>(response);
         }
     }
+=======
+	public class JsonTests
+	{
+		[Fact]
+		public void Can_Deserialize_With_Default_Root() {
+			var doc = CreateJson();
+
+			var d = new JsonDeserializer();
+			var p = d.Deserialize<Person>(doc);
+
+			Assert.Equal("John Sheehan", p.Name);
+			Assert.Equal(new DateTime(2009, 9, 25, 0, 6, 1), p.StartDate);
+			Assert.Equal(28, p.Age);
+			Assert.Equal(long.MaxValue, p.BigNumber);
+			Assert.Equal(99.9999m, p.Percent);
+			Assert.Equal(false, p.IsCool);
+
+			Assert.NotNull(p.Friends);
+			Assert.Equal(10, p.Friends.Count);
+
+			Assert.NotNull(p.BestFriend);
+			Assert.Equal("The Fonz", p.BestFriend.Name);
+			Assert.Equal(1952, p.BestFriend.Since);
+		}
+
+		[Fact]
+		public void Ignore_Protected_Property_That_Exists_In_Data() {
+		}
+
+		[Fact]
+		public void Ignore_ReadOnly_Property_That_Exists_In_Data() {
+		}
+
+		[Fact]
+		public void Can_Deserialize_Names_With_Underscores_On_Default_Root() {
+		}
+
+		private static string CreateJson() {
+			var doc = new JObject();
+			doc["Name"] = "John Sheehan";
+			doc["StartDate"] = new DateTime(2009, 9, 25, 0, 6, 1);
+			doc["Age"] = 28;
+			doc["Percent"] = 99.9999m;
+			doc["BigNumber"] = long.MaxValue;
+			doc["IsCool"] = false;
+			doc["Ignore"] = "dummy";
+			doc["ReadOnly"] = "dummy";
+
+			doc["BestFriend"] = new JObject(
+									new JProperty("Name", "The Fonz"),
+									new JProperty("Since", 1952)
+								);
+
+			var friendsArray = new JArray();
+			for (int i = 0; i < 10; i++) {
+				friendsArray.Add(new JObject(
+									new JProperty("Name", "Friend" + i),
+									new JProperty("Since", DateTime.Now.Year - i)
+								));
+			}
+
+			doc["Friends"] = friendsArray;
+
+			return doc.ToString();
+		}
+	}
+>>>>>>> parent of 449fcad... support for Dictionaries in JsonDeserializer
 }
